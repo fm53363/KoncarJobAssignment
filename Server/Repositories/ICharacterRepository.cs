fc02b35e2fa3619e83035d3d83b39af1822ee627
fmaznik <fran.maznik@gmail.com>
2026-01-06 18:46:04 +0100

    add ICharacterRepository interface and InMemoryCharacterRepository implementation

---
+++ Server/Repositories/ICharacterRepository.cs
@@ -0,0 +1,14 @@
+﻿using Server.Models;
+
+namespace Server.Repositories
+{
+    interface ICharacterRepository
+    {
+        Character? GetById(int id);
+        IEnumerable<Character> GetAll();
+        Character Create(Character character);
+        bool Update(Character character);
+
+        bool Delete(int id);
+    }
+}
---
+++ Server/Repositories/InMemoryCharacterRepository.cs
@@ -0,0 +1,69 @@
+﻿using Server.Models;
+
+namespace Server.Repositories
+{
+    internal class InMemoryCharacterRepository : ICharacterRepository
+    {
+
+        private readonly Dictionary<int, Character> _storage = [];
+        private int _nextId = 0;
+        private readonly object _lock = new();
+
+        public Character Create(Character character)
+        {
+            lock (_lock)
+            {
+                character.Id = _nextId;
+                _storage[_nextId] = character;
+                _nextId++;
+                return character;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            lock (_lock)
+            {
+                if (_storage.ContainsKey(id))
+                {
+                    _storage.Remove(id);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public IEnumerable<Character> GetAll()
+        {
+            lock (_lock)
+            {
+                return _storage.Values.ToList();
+            }
+        }
+
+        public Character? GetById(int id)
+        {
+            lock (_lock)
+            {
+                if (_storage.TryGetValue(id, out Character? value))
+                {
+                    return value;
+                }
+            }
+            return null;
+        }
+
+        public bool Update(Character character)
+        {
+            lock (_lock)
+            {
+                if (_storage.ContainsKey(character.Id))
+                {
+                    _storage[character.Id] = character;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
